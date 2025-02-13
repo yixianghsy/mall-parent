31ae6f967fbaaa53a5fb4ebee70077567de74cdf
yixianghsy <yixianghsy@gmail.com>
2025-02-14 01:25:18 +0800

    add manager+marketing

---
+++ mall-manager/mall-manager-controller/src/main/java/com/mall/ManagerControllerApp.java
@@ -0,0 +1,12 @@
+package com.mall;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ManagerControllerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerControllerApp.class, args);
+    }
+
+}
---
+++ mall-manager/mall-manager-service/src/main/java/com/mall/ManagerServiceApp.java
@@ -0,0 +1,14 @@
+package com.mall;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerServiceApp.class, args);
+    }
+
+}
