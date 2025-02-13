6aaa098acbfa7e6ae112f979a2c4e9c2d0407f4e
yixianghsy <yixianghsy@gmail.com>
2025-02-13 16:32:28 +0800

    add content

---
+++ mall-content/mall-content-servic/src/main/java/com/mall/content/ContentServicApp.java
@@ -0,0 +1,12 @@
+package com.mall.content;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ContentServicApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentServicApp.class, args);
+    }
+
+}
