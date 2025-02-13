a2414055b6a2aaa6e04e9a6ed94cb41f6b20e7f8
yixianghsy <yixianghsy@gmail.com>
2025-02-14 01:30:08 +0800

    add order+portal

---
+++ mall-portal/src/main/java/com/mall/portal/PortalControllerApp.java
@@ -0,0 +1,13 @@
+package com.mall.portal;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PortalControllerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalControllerApp.class, args);
+	}
+
+}
